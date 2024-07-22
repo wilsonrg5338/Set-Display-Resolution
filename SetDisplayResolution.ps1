c5300c4d2d5e6f92be11b1f6ae26a3d7d6859cf1
Reggie Wilson <wilsonrg5338@gmail.com>
2024-07-22 00:59:02 -0500

    Add files via upload

---
+++ SetDisplayResolution.ps1
@@ -0,0 +1,19 @@
+﻿#Author: Reggie Wilson
+#Last Modified: 11/20/2023
+
+#This script will set the display resolution for a computer
+
+#Update the TLS version of the current powershell session
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	
+#Install Nuget Package Provider
+	Install-PackageProvider -Name Nuget -Force
+
+#Install and Import DisplaySettings Module
+    Install-Module -Name DisplaySettings -Force
+    Import-Module -Name DisplaySettings -Force
+
+#Set DisplaySettings
+    Set-DisplayResolution -Width 800 -Height 600
+
+
